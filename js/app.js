6f16a0df02102e81987ef0a6aba67de5b3d3d1e6
SoniGama <abinyuuu@gmail.com>
2018-03-13 13:14:42 -0600

    cambios js y css

--- js/app.js
+++ js/app.js
@@ -21,108 +21,65 @@ getJson = (e) => {
 }
 
 paintDataJson = (json, Json) => {
-    const productsResultsEco = json.results;
-    const productsResultsSust = Json.results;
-    container.innerHTML = '';
+    const products = json.results.concat(Json.results);    
 
-    productsResultsEco.forEach((element, index) => {
-        // console.log(element.id);        
-        if (index <= 50) {
+    products.forEach(element => {    
             let output = `
                 <div class="col-md-3 mb-3">
                     <div class="boxInner">
                         <div class="image">
                             <img src="${element.thumbnail}" alt="${element.title}">
                         </div>
-                        <span class="text">${element.title}</span>
-                        <div class="price">$${element.price} mxn</div>                        
-                        <a href="#modal" data-toggle="modal" class="elements-data" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}">
-                             <i class="fas fa-eye elements-data" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}"></i>
-                        </a>  
-
-                    <div class="buttonShop">Comprar</div>
-                    </div>
+                        <div class="text elements-data">${element.title}</div>
+                        <div class="price">$${element.price} mxn</div> 
+                        <a href="#modal" data-toggle="modal" class="elements-data" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}"> 
+                            <img id="eye" src="assets/images/eye.png" class="elements-data border border-info rounded-circle p-1 mb-1" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}">                    
+                        </a>
+                        <div class="buttonShop">Comprar</div>                   
                 </div>
-            `
-            container.insertAdjacentHTML('beforeend', output);
-        }
+            `   
+            container.insertAdjacentHTML('beforeend', output);        
     })
-
-    productsResultsSust.forEach((element, index) => {
-        // console.log(element.id);
-        if (index <= 50) {
-            let output = `
-                <div class="col-md-3 mb-3">
-                    <div class="boxInner">
-                        <div class="image">
-                            <img src="${element.thumbnail}" alt="${element.title}">
-                        </div>
-
-                        <div class="text">${element.title}</div>
-                        <div class="price">$${element.price} mxn</div>
-                        <a href="#modal" data-toggle="modal" class="elements-data" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}">
-                            <i class="fas fa-eye elements-data" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}"></i>
-                        </a> 
-
-                    
-
-                    <div class="buttonShop">Comprar</div>
-                    </div>
-                </div>
-            `
-            container.insertAdjacentHTML('beforeend', output);
-        }
-    })
-
     let elementsData = document.getElementsByClassName('elements-data');
     elementEvent(elementsData);
 }
 
-
 elementEvent = (elementsData) => {
     let elementsEvents = Array.from(elementsData);
-
     elementsEvents.forEach(button => {
         button.addEventListener('click', getInfo);
-
     })
 }
 
 getInfo = (e) => {
     e.preventDefault();
-
-    const img = e.target.dataset.img;
-    const title = e.target.dataset.title;
-    const rating = e.target.dataset.rating;
-    const state = e.target.dataset.state;
-    const price = e.target.dataset.price;
+    const dataSet = e.target.dataset;
 
     fetch(`https://api.mercadolibre.com//items/${e.target.dataset.id}/description`).then(response => {
         response.json().then(json => {
             const description = json.plain_text;
-            paintInfoModal(img, title, rating, state, description, price);
-
+            paintInfoModal(dataSet, description);
         })
     })
 }
 
-paintInfoModal = (img, title, rating, state, description, price) => {
+paintInfoModal = (dataSet, description) => {
     const containerImg = document.getElementById('image');
     const titleStars = document.getElementById('description-modal');
     const modalBody = document.getElementById('modal-body');
 
-    containerImg.innerHTML = `<img src="${img}" alt="${title}">`;
+    containerImg.innerHTML = `<img src="${dataSet.img}" alt="${dataSet.title}">`;
 
     titleStars.innerHTML = `
-                        <h5 class="modal-title">${title}</h5>
-                        <div class="stars">${rating}
+                        <h5 class="modal-title">${dataSet.title}</h5>
+                        <div class="stars">${dataSet.rating}
                             <i class="fas fa-star"></i>                           
                         </div>         
     `;
 
     modalBody.innerHTML = `
-                        <p>Lugar de origen: ${state}</p>
+                        <p>Lugar de origen: ${dataSet.state}</p>
                         <p>${description}</p>
-                        <div class="priceProduct">$ ${price} mxn</div>
+                        <div class="priceProduct">$ ${dataSet.price} mxn</div>
     `;
 }
