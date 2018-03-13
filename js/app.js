91d8905e944c16385dae9486dabac6906b7d9890
ximada <witch_ada@hotmail.com>
2018-03-12 22:38:17 -0600

    funcion agregar al carrito

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+//Add the car
+const addProducts = document.getElementById('products');
+
+ addCar = ()=> {
+    e.preventDefaul();
+     
+    if(e.target.classList.contains('add-car')){
+         const product = e.target.parentElement.parentElement;
+
+
+    }
+}
+
+addProductsCar = (addProducts)=>{
+    
+
+}
+
+
