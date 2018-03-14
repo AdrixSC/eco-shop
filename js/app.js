42cf7bd991d0d1fac0ceed7ef9cdffe00f5bb604
SoniGama <abinyuuu@gmail.com>
2018-03-14 15:12:40 -0600

    cambios js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-
-//Add the car
-// const addProducts = document.getElementById('products');
-
-// addCar = () => {
-//     e.preventDefaul();
-
-//     if (e.target.classList.contains('add-car')) {
-//         const product = e.target.parentElement.parentElement;
-
-//     }
-// }
-
-// addProductsCar = (addProducts) => {
-
-
-// }
-
-
-
 const container = document.getElementById('container');
 window.addEventListener('load', () => {
     getJson();
@@ -39,12 +19,6 @@ getJson = (e) => {
         });
 }
 
-// const data = (json, Json) => {
-//     const dataArray = json.results.concat(Json.results);
-//     console.log(dataArray);    
-//     return dataArray;
-// }
-
 paintDataJson = (json, Json) => {
     const products = json.results.concat(Json.results);
     const productsData = localStorage.setItem('products', JSON.stringify(products));
@@ -61,9 +35,7 @@ paintDataJson = (json, Json) => {
                         <a href="#modal" data-toggle="modal" class="elements-data" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}"> 
                             <img id="eye" src="assets/images/eye.png" class="elements-data border border-info rounded-circle p-1 mb-1" data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" data-id="${element.id}" data-rating="${element.reviews.rating_average}" data-state="${element.address.state_name}">                    
                         </a>
-                  
-                        <div data-price="${element.price}" data-title="${element.title}" data-img="${element.thumbnail}" class="buttonShop">Agregar a carrito</div>                   
-
+                        <div data-id="${element.id}" class="buttonShop">Agregar a carrito</div>
                 </div>
             `
         container.insertAdjacentHTML('beforeend', output);
@@ -92,14 +64,10 @@ filterProducts = ()=>{
     }
 searchInput.addEventListener('keyup', filterProducts);
 
-paintProducsFilters = (prueba)=>{
-    console.log(prueba);
-    
-    
-    
+paintProducsFilters = (prueba)=>{ 
 
-     container.innerText = '';
-     prueba.forEach(producSelected => {
+    container.innerText = '';
+    prueba.forEach(producSelected => {
 
         let templateFilters = `
                 <div class="col-md-3 mb-3">
@@ -112,8 +80,7 @@ paintProducsFilters = (prueba)=>{
                         <a href="#modal" data-toggle="modal" class="elements-data" data-price="${producSelected.price}" data-title="${producSelected.title}" data-img="${producSelected.thumbnail}" data-id="${producSelected.id}" data-rating="${producSelected.reviews.rating_average}" data-state="${producSelected.address.state_name}"> 
                             <img id="eye" src="assets/images/eye.png" class="elements-data border border-info rounded-circle p-1 mb-1" data-price="${producSelected.price}" data-title="${producSelected.title}" data-img="${producSelected.thumbnail}" data-id="${producSelected.id}" data-rating="${producSelected.reviews.rating_average}" data-state="${producSelected.address.state_name}">                    
                         </a>
-                  
-                        <div data-price="${producSelected.price}" data-title="${producSelected.title}" data-img="${producSelected.thumbnail}" class="buttonShop">Agregar a carrito</div>                   
+                        <div data-id"${producSelected.id}" class="buttonShop">Agregar a carrito</div>                   
                 </div>
             `
         container.insertAdjacentHTML('beforeend', templateFilters);
@@ -122,10 +89,6 @@ paintProducsFilters = (prueba)=>{
     elementEvent(elementsData);
     let buttonAddCart = document.getElementsByClassName('buttonShop');
     converToArray(buttonAddCart);
-
-
-
-
 }
 
 converToArray = (buttonAddCart) => {
@@ -171,7 +134,10 @@ filterProducts = (productsSelection) => {
         });
     });
 
-    paintCart(products);    
+    console.log(products);
+    
+
+    // paintCart(products);    
 }
 
 
@@ -190,78 +156,13 @@ removeCounter = () => {
     counter.innerHTML = counterItems;
 }
 
-chageButtonStatus = (e) => {
-    const button = e.target;
-    if (button.innerText == 'Agregar a carrito') {
-        addCounter();
-        addItems(button);
-        button.innerText = 'Quitar del carrito';
-    } else {
-        removeCounter();
-        removeItems(button);
-        button.innerText = 'Agregar a carrito';
-    }
-}
-const arrayCart = [];
-addItems = (button) => {
-    const containerCart = document.getElementById('container-cart');
-    const objItems = {
-        img: button.dataset.img,
-        title: button.dataset.title,
-        price: button.dataset.price,
-        id: button.dataset.id
-    }
-
-    arrayCart.push(objItems);
-
-    // delete wooooooo
-
-    // const indexObj = arrayCart.indexOf(objItems);
-    // arrayCart.splice(indexObj, 1);
-    // console.log(arrayCart);
-
-
-    // let cartTemplate = ``;
-    // cartTemplate += `
-    //                 <div class="row boxGeneral">
-    //                     <div class="col-md-6">
-    //                         <div class="imageProduct">
-    //                             <img src="${img}" alt="${title}">
-    //                         </div>
-    //                     </div>
-    //                     <div class="col-md-6">
-    //                         <div class="titleProduct">${title}</div>
-    //                         <div class="priceProduct">Precio Unitario $${price}</div>                                                      
-    //                     </div>
-    //                 </div>    
-    // `;
-    // containerCart.insertAdjacentHTML('beforeend', cartTemplate);    
-}
-removeItems = (button) => {
-    // console.log(button);
-    // const indexObj = arrayCart.indexOf(objItems);
-    // arrayCart.splice(indexObj, 1);
-    // console.log(arrayCart);
-
-}
-addCounter = () => {
-    let counterItems = parseInt(document.getElementById('counter-items').innerText);
-    let counter = document.getElementById('counter-items');
-    counterItems += 1;
-    counter.innerHTML = counterItems;
-}
-removeCounter = () => {
-    let counterItems = parseInt(document.getElementById('counter-items').innerText);
-    let counter = document.getElementById('counter-items');
-    counterItems -= 1
-    counter.innerHTML = counterItems;
-}
 elementEvent = (elementsData) => {
     let elementsEvents = Array.from(elementsData);
     elementsEvents.forEach(button => {
         button.addEventListener('click', getInfo);
     })
 }
+
 getInfo = (e) => {
     e.preventDefault();
     const dataSet = e.target.dataset;
@@ -272,6 +173,7 @@ getInfo = (e) => {
         })
     })
 }
+
 paintInfoModal = (dataSet, description) => {
     const containerImg = document.getElementById('image');
     const titleStars = document.getElementById('description-modal');
